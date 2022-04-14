e6f51ab690942f9f3468670bdabd0c1c238022fa
Yeob <happyk100@naver.com>
2022-04-14 15:38:11 +0900

    1032.c

--- Baekjoon/C/Bronze_I/1032.c
+++ Baekjoon/C/Bronze_I/1032.c
@@ -1,40 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-    int num;
-    int length;
-    char buffer[50]; // buffer에 먼저 입력받은 후 문자열 길이만큼 새로운 공간 할당해서 저장.
+int main(){
+    int num=0;
+    int length=0;
+    int i=0;
+    char input[50][51];
+    char ans[51];
 
     scanf("%d", &num);
-    char* arr = (char*)malloc(sizeof(char)*num);
-    for(int i=0; i<num; i++){
-        scanf("%s", buffer);
-        length = strlen(buffer);
-        char* fileName = (char*) malloc(sizeof(char)*(length+1));
-        strcpy(fileName, buffer);
-        arr[i] = fileName;
-    }
-    
-    int* check = (int*) malloc(sizeof(int)*length);
+    for(i=0; i<num; i++)
+        scanf("%s", input[i]);
 
-    for(int i=0; i<length; i++){
-        for(int j=0; j<num-1; j++){
-            if(arr[j][i]=arr[j+1][i]){
-                check[i]=1;
-            }else{
-                check[i]=0;
-                break;
-            }
-        }
-    }
+    length = strlen(input[0]);
+    strcpy(ans, input[0]);
+    for(int i=1; i<num; i++)
+        for(int j=0; j<length; j++)
+            if(ans[j]!='?' && ans[j]!=input[i][j])
+                ans[j] = '?';
+    printf("%s", ans);
 
-    for(int i=0; i<length; i++){
-        if(check[i]==1){
-            printf("%s",arr[0][i]);
-        }else{
-            printf("?");
-        }
-    }
     return 0;
 }
