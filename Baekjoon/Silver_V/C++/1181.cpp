f311d81493882a4c49ebd2ae214a3b4f315b0430
Yeob <happyk100@naver.com>
2022-04-27 23:57:36 +0900

    1181.cpp

--- Baekjoon/Silver_V/C++/1181.cpp
+++ Baekjoon/Silver_V/C++/1181.cpp
@@ -12,14 +12,30 @@ int main(){
 
     for(int i=0; i<size; i++){
         cin >> arr[i];
+        for(int j=0; j<size; j++){
+            if(arr[i] == arr[j]){
+                i--;
+                size--;
+                break;
+            }
+        }
     }
 
-    for(int i=1; i<size; i++){
-        min = 0;
-        if(arr[i].length() < arr[min].length())
-            min = i;
-        if(arr[i].length() == arr[min].length())
-    }
+    while(size!=1)
+        for(int i=1; i<size; i++){
+            min = 0;
+            if(arr[i].length() < arr[min].length())
+                min = i;
+            else if(arr[i].length() == arr[min].length())
+                if(arr[i] < arr[min])
+                    min = i;
+            if(i==size-1){
+                cout << arr[min] << "\n";
+                arr[min] = arr[i];
+                size --;
+            }
+        }
     
+
     return 0;
 }
